fa4625483028962df910fcd937502d25e54ea68b
mishina naoya <msnyxx811@gmail.com>
2023-07-01 13:58:19 +0900

    ルートパラメータのテスト

---
+++ resources/js/Pages/Inertia/Show.vue
@@ -0,0 +1,9 @@
+<script setup>
+defineProps({ // 変数の受け取り
+  id: String 
+})
+</script>
+
+<template>
+  {{ id }}
+</template>
--- resources/js/Pages/InertiaTest.vue
+++ resources/js/Pages/InertiaTest.vue
@@ -6,5 +6,6 @@
   Inertiaテストです。<br>
   <a href="/">aタグでWelcomeに移動</a><br>
   <Link href="/">LinkでWelcomeに移動</Link><br>
-  <Link :href="route('inertia.index')">名前付きルート</Link>
+  <Link :href="route('inertia.index')">名前付きルート</Link><br>
+  <Link :href="route('inertia.show', { id : 1 })">ルートパラメータの確認</Link><br>
 </template>
