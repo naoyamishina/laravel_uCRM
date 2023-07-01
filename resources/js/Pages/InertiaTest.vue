23f1e9ccc06b7926b299fb3d33b9151237fa0042
mishina naoya <msnyxx811@gmail.com>
2023-07-01 13:41:09 +0900

    名前付きルートのテスト

--- resources/js/Pages/InertiaTest.vue
+++ resources/js/Pages/InertiaTest.vue
@@ -5,5 +5,6 @@
 <template>
   Inertiaテストです。<br>
   <a href="/">aタグでWelcomeに移動</a><br>
-  <Link href="/">LinkでWelcomeに移動</Link>
+  <Link href="/">LinkでWelcomeに移動</Link><br>
+  <Link :href="route('inertia.index')">名前付きルート</Link>
 </template>
