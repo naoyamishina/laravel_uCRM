399691e4844eec053eefc4bac59456cd0800bb26
mishina naoya <msnyxx811@gmail.com>
2023-07-06 20:12:23 +0900

    購入保存

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StorePurchaseRequest;
 use App\Http\Requests\UpdatePurchaseRequest;
+use App\Models\Item;
 use App\Models\Purchase;
 use Inertia\Inertia;
 use App\Models\Customer; 
-use App\Models\Item;
+use Illuminate\Support\Facades\DB;
 
 class PurchaseController extends Controller
 {
@@ -46,7 +47,27 @@ class PurchaseController extends Controller
      */
     public function store(StorePurchaseRequest $request)
     {
-        //
+        DB::beginTransaction();
+        
+        try{
+            $purchase = Purchase::create([ 
+                'customer_id' => $request->customer_id, 
+                'status' => $request->status,
+            ]);
+
+            foreach($request->items as $item)
+            { 
+                $purchase->items()->attach(
+                    $item['id'],
+                    ['quantity' => $item['quantity']]
+                );
+            }
+            DB::commit();
+        
+            return to_route('dashboard');
+        } catch(\Exception $e){ 
+            DB::rollback();
+        }
     }
 
     /**
