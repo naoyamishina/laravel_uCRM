b4507732fc685e3c20d1dc42902aa796951b2817
mishina naoya <msnyxx811@gmail.com>
2023-07-07 20:17:08 +0900

    emit

--- resources/js/Components/MicroModal.vue
+++ resources/js/Components/MicroModal.vue
@@ -1,46 +1,51 @@
 <script setup>
-import { ref, reactive } from 'vue';
-import axios from 'axios'
-import { onMounted } from 'vue'
+import axios from 'axios';
+import { ref, reactive, onMounted } from 'vue'
 
 const search = ref('')
 const customers = reactive({})
 
 // onMounted(() => {
-//   console.log(customers) 
+//   axios.get('/api/user')
+//   .then( res => {
+//     console.log(res.data)
+//   })
 // })
 
 const isShow = ref(false)
-
-const toggleStatus = () => { 
-  isShow.value = !isShow.value
-}
+const toggleStatus = () => { isShow.value = !isShow.value}
 
 const searchCustomers = async () => {
-  try{
+  try {
   await axios.get(`/api/searchCustomers/?search=${search.value}`)
-    .then( res => { 
-      console.log(customers) 
-      console.log(res.data) 
-      customers.value = res.data
-      console.log(customers) 
-    }) 
-    toggleStatus() 
-  } catch (e){
-    console.log(e.message)
+  .then( res => {
+    console.log(res.data)
+    customers.value = res.data
+  })
+  toggleStatus()
+  } catch(e){
+    console.log(e)
   }
 }
-</script> 
 
+const emit = defineEmits(['update:customerId'])
+
+const setCustomer = e => {
+  search.value = e.kana
+  emit('update:customerId', e.id)
+  toggleStatus()
+}
+
+</script>
 <template>
   <div v-show="isShow" class="modal" id="modal-1" aria-hidden="true">
     <div class="modal__overlay" tabindex="-1" data-micromodal-close>
       <div class="modal__container w-2/3" role="dialog" aria-modal="true" aria-labelledby="modal-1-title">
         <header class="modal__header">
           <h2 class="modal__title" id="modal-1-title">
-            Micromodal
+            顧客検索
           </h2>
-          <button type="button" @click="toggleStatus" class="modal__close" aria-label="Close modal" data-micromodal-close></button>
+          <button @click="toggleStatus" type="button" class="modal__close" aria-label="Close modal" data-micromodal-close></button>
         </header>
         <main class="modal__content" id="modal-1-content">
           <div v-if="customers.value" class="lg:w-2/3 w-full mx-auto overflow-auto">
@@ -67,14 +72,14 @@ const searchCustomers = async () => {
               </tr>
             </tbody>
           </table>
-          </div>
+        </div>
         </main>
         <footer class="modal__footer">
-          <button type="button" @click="toggleStatus" class="modal__btn" data-micromodal-close aria-label="Close this dialog window">Close</button>
+          <button @click="toggleStatus" type="button" class="modal__btn" data-micromodal-close aria-label="Close this dialog window">閉じる</button>
         </footer>
       </div>
     </div>
   </div>
-  <input name="customer" v-model="search">
-  <button type="button" @click="searchCustomers" data-micromodal-trigger="modal-1" href='javascript:;'>Open Modal Dialog</button >
+  <input name="customer" v-model="search" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+  <button @click="searchCustomers" type="button" data-micromodal-trigger="modal-1" class="flex mx-auto text-white bg-teal-500 border-0 py-2 px-8 focus:outline-none hover:bg-teal-600 rounded text-lg">検索する</button>
 </template>
