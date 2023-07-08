40f35810e84f9af2e4c0df02b98596e3b8440d6a
mishina naoya <msnyxx811@gmail.com>
2023-07-08 15:49:30 +0900

    carbon

--- app/Http/Controllers/Api/AnalysisController.php
+++ app/Http/Controllers/Api/AnalysisController.php
@@ -6,13 +6,32 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Order;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 
 class AnalysisController extends Controller
 {
     public function index(Request $request) {
-        // Ajax通信なのでJsonで返却する必要がある 
-        return response()->json([
-            'data' => $request->startDate // 仮設定 
+        $subQuery = Order::betweenDate($request->startDate, $request->endDate);
+
+        if($request->type === 'perDay') {
+            $subQuery->where('status', true)->groupBy('id')->selectRaw(
+            'SUM(subtotal) AS totalPerPurchase, 
+            DATE_FORMAT(created_at, "%Y%m%d") AS date')
+            ->groupBy('date');
+        
+            $data = DB::table($subQuery)
+            ->groupBy('date')
+            ->selectRaw('date, sum(totalPerPurchase) as total') 
+            ->get();
+        }
+        return response()->json([ 
+            'data' => $data,
+            'type' => $request->type 
         ], Response::HTTP_OK);
+        
+        // Ajax通信なのでJsonで返却する必要がある 
+        // return response()->json([
+        //     'data' => $request->startDate // 仮設定 
+        // ], Response::HTTP_OK);
     }
 }
